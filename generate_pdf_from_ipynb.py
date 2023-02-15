afe844bf7ba4e3b6b31998918eb61e7e2e3fa63c
Alex Fok <alexfokil@gmail.com>
2023-02-15 22:44:59 +0200

    Update generate_pdf_from_ipynb.py

--- generate_pdf_from_ipynb.py
+++ generate_pdf_from_ipynb.py
@@ -1,11 +1,12 @@
+notebook_name= 'IDL_Project_v1'
+
 !apt-get install texlive texlive-xetex texlive-latex-extra pandoc > /dev/null 2>&1
 !pip install pypandoc > /dev/null 2>&1
-
 from google.colab import drive
 drive.mount('/content/drive')
-!cp 'drive/My Drive/Colab Notebooks/Assignment3_work.ipynb' ./
+!cp 'drive/My Drive/Colab Notebooks/'$notebook_name'.ipynb' ./
 
-!jupyter nbconvert --to PDF "Assignment3_work.ipynb" > /dev/null 2>&1
+!jupyter nbconvert --to PDF $notebook_name'.ipynb' > /dev/null 2>&1
 !echo "pdf file generated"
-#!ls -la
-!cp './Assignment3_work.pdf' 'drive/My Drive/Colab Notebooks'
+!ls -la
+!cp './'$notebook_name'.pdf' 'drive/My Drive/Colab Notebooks'
